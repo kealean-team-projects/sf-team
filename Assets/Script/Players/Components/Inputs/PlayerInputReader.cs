afbd4722ffc4b8ffeac7bfd67f28f902559338f9
kealean <uiseunghan62@gmail.com>
2026-07-08 19:33:53 +0900

    update

--- Assets/Script/Players/Components/Inputs/PlayerInputReader.cs
+++ Assets/Script/Players/Components/Inputs/PlayerInputReader.cs
@@ -1,7 +1,25 @@
 ﻿using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace Script.Players.Components.Inputs {
-    public class PlayerInputReader : MonoBehaviour {
-        
+    public class PlayerInputReader : MonoBehaviour, Controls.IPlayerActions {
+        public void OnMove(InputAction.CallbackContext context) { 
+            
+        }
+        public void OnLook(InputAction.CallbackContext context) {
+            
+        }
+        public void OnInteract(InputAction.CallbackContext context) {
+            
+        }
+        public void OnCrouch(InputAction.CallbackContext context) {
+            
+        }
+        public void OnJump(InputAction.CallbackContext context) {
+            
+        }
+        public void OnSprint(InputAction.CallbackContext context) {
+            
+        }
     }
 }
