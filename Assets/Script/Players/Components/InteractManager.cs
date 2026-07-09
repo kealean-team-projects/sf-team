37b2fe8a091a6f8c379bc77945604ecc17595bab
tyu49 <k10y4j9@gmail.com>
2026-07-10 00:49:15 +0900

    interact

--- Assets/Script/Players/Components/InteractManager.cs
+++ Assets/Script/Players/Components/InteractManager.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using Unity.U2D.Physics;
 using UnityEngine;
 using UnityEngine.UIElements;
@@ -10,36 +11,62 @@ namespace Script.Players.Components
         [SerializeField] private float xSize;
         [SerializeField] private float ySize;
         [SerializeField] private float zSize;
+        [SerializeField] private float xPos;
+        [SerializeField] private float yPos;
+        [SerializeField] private float zPos;
         [SerializeField] private PhysicsShape.ContactFilter whatIsTarget;
         [SerializeField] private bool debug;
-
+        [SerializeField] private int bufferSize;
+        
         private Vector3 BoxSize => new Vector3(xSize, ySize, zSize);
+        private Vector3 BoxPos => transform.position + new Vector3(xPos, yPos, zPos);
         private Collider[] _interactArray;
+        
 
+        private void Awake()
+        {
+            _interactArray = new Collider[bufferSize];
+        }
+
+        [ContextMenu("afs")]
         public void Interact()
         {
-            _interactArray = null;
-            int count = Physics.OverlapBoxNonAlloc(transform.position, BoxSize*0.5f, _interactArray,
-                Quaternion.identity, whatIsTarget.groupIndex);
+            int count = Physics.OverlapBoxNonAlloc(BoxPos, BoxSize*0.5f, _interactArray,
+                transform.rotation, whatIsTarget.groupIndex);
             if (count <= 0) return;
+            float distance = float.MaxValue;
+            IInteractable closestInteractor = null;
             for (int i = 0; i < count; i++)
             {
-                if (_interactArray != null && _interactArray[i].TryGetComponent<IInteractable>(out var interact))
+                var collider = _interactArray[i];
+                var rb = collider.attachedRigidbody;
+                if (rb && rb.TryGetComponent<IInteractable>(out var interact) || collider.TryGetComponent(out interact))
                 {
-                    interact.Interact();
-                    return;
+                    float currentDistance = (collider.transform.position - transform.position).sqrMagnitude;
+                    if (currentDistance >= distance) continue;
+                    distance = currentDistance;
+                    closestInteractor = interact;
                 }
+                
             }
-            
-            Debug.LogWarning("감지된 물체중 IInteractable을 가진 물체가 없음");
+
+            if (closestInteractor == null)
+            {
+                Debug.LogWarning("감지된 물체중 IInteractable을 가진 물체가 없음");
+                return;
+            }
+            closestInteractor.Interact();
         }
 
         private void OnDrawGizmos()
         {
             if (!debug) return;
             Gizmos.color = Color.red;
-            Gizmos.DrawWireCube(transform.position, BoxSize);
+            Gizmos.matrix = transform.localToWorldMatrix;
+            Gizmos.DrawWireCube(Vector3.zero + new Vector3(xPos, yPos, zPos), BoxSize);
         }
+        
+        
     }
 
     internal interface IInteractable
