e321e2ceb13bfbb39a2d072f15626e7e5d9a8197
tyu49 <k10y4j9@gmail.com>
2026-07-14 19:17:31 +0900

    CanPickupObject Interact abstract class

--- Assets/Script/Interectable-Object/TestInteract.cs
+++ Assets/Script/Interectable-Object/TestInteract.cs
@@ -1,10 +1,11 @@
+using Script.Players.Components;
 using UnityEngine;
 
 namespace Script.Interectable_Object
 {
     public class TestInteract : MonoBehaviour, IInteractable
     {
-        public void Interact()
+        public void Interact(InteractManager owner)
         {
             Debug.Log("interacted");
         }
