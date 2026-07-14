9ae2efbb5de471a32c59bd773ff8ce0024f93601
tyu49 <k10y4j9@gmail.com>
2026-07-14 19:37:05 +0900

    pickkkkkkkkkkkkkkkkkkkkkuppppppppppppppppppppppppp

--- Assets/Script/Interectable-Object/AbstractInteractable/CanPickupObject.cs
+++ Assets/Script/Interectable-Object/AbstractInteractable/CanPickupObject.cs
@@ -6,17 +6,26 @@ namespace Script.Interectable_Object.AbstractInteractable
 {
     public abstract class CanPickupObject : MonoBehaviour, IInteractable
     {
+        private Rigidbody _rb;
         private Transform _handPos;
         private bool _isInHand;
+
+        private void Awake()
+        {
+            _rb = GetComponent<Rigidbody>();
+        }
+
         public void Interact(InteractManager owner)
         {
             if (_isInHand)
             {
                 _isInHand = false;
+                _rb.useGravity = true;
                 return;
             }
 
             _handPos = owner.HandPos;
+            _rb.useGravity = false;
             _isInHand = true;
         }
 
