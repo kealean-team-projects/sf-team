34d19fd674a7dbe9f77b32e744470700aa04ffa0
tyu49 <k10y4j9@gmail.com>
2026-07-16 17:50:13 +0900

    MovingInteractorMakeStart

---
+++ Assets/Script/Interectable-Object/AbstractInteractable/MovingInteractor.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+using Script.Players.Components;
+using UnityEngine;
+
+namespace Script.Interectable_Object.AbstractInteractable
+{
+    public class MovingInteractor : MonoBehaviour, IInteractable
+    {
+        
+        
+        public void Interact(InteractManager owner)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
